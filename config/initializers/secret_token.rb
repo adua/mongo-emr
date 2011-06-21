8645720fc41a19190b4f0c8e50c55a9a4fd4891f
Ashish Dua <adua89@gmail.com>
2011-06-21 16:16:09 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoEmr::Application.config.secret_token = '34f5ca5c079c7ff8f2e4a4a9e395618d9eb296d6a1f768c62bb12f101d509542f54a3392a4c165ca90b995f84ffb7f2386f46ab1d49ae05f2f8e2cdd846da7b5'
